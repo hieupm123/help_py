22f3b6f36d6bc34868bee2fe668cd39f307bc36d
your_github_username <hello@hlleo>
2022-02-27 00:14:51 +0700

    first commit

--- a.py
+++ a.py
@@ -2,9 +2,12 @@ n= int(input());
 m= int(input());
 # 10<=n<=m<= 10^9
 ans = 0;
+pre = 1;
 
-while n < m:
-	n = int(n/10) + n;
-	ans += 1; 
+# ~300 AC
+
+while(float(n * pre) < m):
+	ans += 1;
+	pre = float(pre * (1.1));
 
 print(ans);
